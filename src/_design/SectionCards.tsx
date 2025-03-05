4cafc660a7d0b95748dd5953aee212295e6d735e
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2025-03-05 12:42:39 +0100

    Change styling of SectionCards.tsx
    Added some more properties to the Section Cards template, including
    upper border on each child to work as a separator, and also bg colour
    to differentiate between them. Ultimately I think it would be ideal
    if we can incorporate team colors to the background for each child,
    I have manually set FSU color in this instance to see how it looks.

--- src/_design/SectionCards.tsx
+++ src/_design/SectionCards.tsx
@@ -1,28 +1,34 @@
 import React, { ReactElement } from "react";
 import { Text } from "./Typography";
-import { ColFrame } from "../components/Common/Frame";
 
 interface SectionCardsProps {
   header: string | ReactElement;
   children: React.ReactNode;
+  classes?: string;
 }
 
 export const SectionCards: React.FC<SectionCardsProps> = ({
   header,
   children,
+  classes="",
 }) => {
   return (
-    <ColFrame className="flex-1 overflow-y-scroll max-h-[50vh] border-4 rounded-xl scroll-p-2 scroll-smooth">
-      <div className="flex flex-col rounded-lg p-4 shadow-md">
-        <div className="mb-4">
-          <Text variant="h4" className="font-bold">
-            {header}
-          </Text>
-        </div>
-        <div className="flex flex-col">
-          {children}
-        </div>
+    <div className={`flex flex-col rounded-lg p-4 shadow-md outline w-[20em] h-[20em] max-w-[20em] max-h-[20em] ${classes}`}>
+      <div className="mb-4">
+        <Text variant="h4" className="font-bold">
+          {header}
+        </Text>
       </div>
-    </ColFrame>
+      <div className="flex flex-col overflow-y-scroll h-full">
+        {React.Children.map(children, (child, index) => (
+          <div
+            className={index % 2 === 0 ? "border-t" : "bg-gray-800 border-t"}
+            style={{ backgroundColor: index % 2 === 0 ? "#782F40" : "transparent" }}
+          >
+            {child}
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
