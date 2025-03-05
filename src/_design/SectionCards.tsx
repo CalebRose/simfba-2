2bfae02bbec5910e785f4ad72f7202b2614993fb
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2025-03-05 08:35:53 +0100

    Commit in order to move to main pull updates and merge main to my branch

--- src/_design/SectionCards.tsx
+++ src/_design/SectionCards.tsx
@@ -1,9 +1,10 @@
-import React, { ReactNode } from "react";
+import React, { ReactElement } from "react";
 import { Text } from "./Typography";
+import { ColFrame } from "../components/Common/Frame";
 
 interface SectionCardsProps {
-  header: string;
-  children: ReactNode;
+  header: string | ReactElement;
+  children: React.ReactNode;
 }
 
 export const SectionCards: React.FC<SectionCardsProps> = ({
@@ -11,15 +12,17 @@ export const SectionCards: React.FC<SectionCardsProps> = ({
   children,
 }) => {
   return (
-    <div className="flex rounded-lg">
-      <div className="mb-4">
-        <Text variant="h4" className="font-bold">
-          {header}
-        </Text>
+    <ColFrame className="flex-1 overflow-y-scroll max-h-[50vh] border-4 rounded-xl scroll-p-2 scroll-smooth">
+      <div className="flex flex-col rounded-lg p-4 shadow-md">
+        <div className="mb-4">
+          <Text variant="h4" className="font-bold">
+            {header}
+          </Text>
+        </div>
+        <div className="flex flex-col">
+          {children}
+        </div>
       </div>
-      <div className="flex">
-        {children}
-      </div>
-    </div>
+    </ColFrame>
   );
 };
